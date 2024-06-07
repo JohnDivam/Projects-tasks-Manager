f7008cb8737958b7645e6b7b626d13529247da7c
Mohammad Salam <m.salam@houya.io>
2024-06-07 02:52:26 +0100

    first commit

---
+++ src/components/layouts/AppLayout.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="AppLayout">
+        <AppNavbar />
+        <main>
+          <slot></slot>
+        </main>
+        <footer>
+          <div class="container">
+      <div class="row">
+        <div class="col-md-12 text-center">
+          <p>&copy; 2024 Task Management. All rights reserved.</p>
+        </div>
+      </div>
+    </div>
+        </footer>
+    </div>
+</template>
+
+
+<script>
+import AppNavbar from './AppNavbar.vue'
+export default {
+  components:{
+    AppNavbar
+  },
+  props: {
+    pageTitle: String
+  }
+}
+</script>
+
+<style scoped>
+footer {
+  background-color: #f8f9fa;
+  padding: 20px 0;
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,18 @@
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
+import { createVuetify } from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'; // Import Vuetify CSS
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
+const vuetify = createVuetify({
+    components,
+    directives,
+});
+
+app = createApp(App);
+app.use(vuetify);
+app.mount('#app');
