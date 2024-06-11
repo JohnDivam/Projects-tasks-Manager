4b84f7e7dc89081e0bdffee4a14c095649f2da72
Mohammad Salam <m.salam@houya.io>
2024-06-11 23:41:35 +0100

    Add redirectAfterLogin

--- src/components/User/Home.vue
+++ src/components/User/Home.vue
@@ -1,3 +1,22 @@
 <template>
-    dd
+        <DashLayout page-title="Home">
+        <div class="homepage">
+                <h1>Welcome to Task Management</h1>
+        </div>
+        </DashLayout>
 </template>
+
+
+<script>
+
+import DashLayout from '../layouts/DashLayout.vue';
+
+export default {
+    components:{
+        DashLayout,
+    },
+    setup() {
+         
+    },
+}
+</script>
